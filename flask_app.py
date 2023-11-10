0c0565d9cf2fd609397559d39c637cfba0de1f64
madrow1 <rowan.pitt@btinternet.com>
2023-11-10 13:15:11 +0000

    uooih

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request, jsonify
 
-mist_automation = Flask(__name__)
+app = Flask(__name__)
 
-mist_automation.route("/")
+@app.route("/")
 def index():
     return render_template("index.html")
 
-mist_automation.run()
+app.run()
