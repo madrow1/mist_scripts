09e2d927d50227a8a9864588dda80b2db2590df1
madrow1 <rowan.pitt@btinternet.com>
2023-11-10 14:03:12 +0000

    flask basically operational

--- mist_flask/__init__.py
+++ mist_flask/__init__.py
@@ -35,4 +35,8 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
+    from . import apps
+    app.register_blueprint(apps.bp)
+    app.add_url_rule('/', endpoint='index')
+
     return app
